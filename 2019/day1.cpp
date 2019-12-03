cb6638c399d786fc440d057a6fccac9c78aa14dc
Florian Anderiasch <fa@art-core.org>
2019-12-03 22:03:50 +0100

    2019 - 1.2

--- 2019/day1.cpp
+++ 2019/day1.cpp
@@ -30,6 +30,7 @@ int main(int argc, char * argv[])
 	std::string input;
 	std::string::size_type sz;
 	int total = 0;
+	int total2 = 0;
 	while (std::cin) {
 		getline(std::cin, input);
 		if (input.size() < 2) continue;
@@ -37,6 +38,15 @@ int main(int argc, char * argv[])
 		int num = std::stoi(input, &sz);
 		int num2 = cf(num);
 		total += num2;
+
 		std::cout << " = " << num << " => " << num2 << " :: " << total << std::endl;
+
+		int fuel = num2;
+		do {
+			fuel = cf(fuel);
+			if (fuel > 0) total2 += fuel;
+		} while (fuel > 0);
+
+		std::cout << " = " << num << " => " << num2 << " :: " << total2 << " :: " << (total + total2)<< std::endl;
 	}
 }
