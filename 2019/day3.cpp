955de4375245bbdd056133f7d9a7bd8a1c17efb8
Florian Anderiasch <fa@art-core.org>
2019-12-05 06:59:38 +0100

    2019 - 3.1 slow

--- 2019/day3.cpp
+++ 2019/day3.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <iostream>
 #include <sstream>
 #include <tuple>
@@ -5,6 +6,17 @@
 
 /*
 */
+
+void pprint(std::vector<std::tuple<int,int>> v, std::string prefix = "")
+{
+	return;
+	if (prefix.size() > 0) std::cout << prefix << " :: ";
+	for (auto it = v.begin(); it != v.end(); ++it) {
+		std::tuple<int,int> x = *it;
+		std::cout << "(" << std::get<0>(x) << "/" << std::get<1>(x) << ")";
+	}
+	std::cout << std::endl;
+}
 
 std::vector<std::tuple<int,int>> go(std::tuple<std::string,int> s, std::tuple<int,int> origin)
 {
@@ -30,13 +42,13 @@ std::vector<std::tuple<int,int>> go(std::tuple<std::string,int> s, std::tuple<in
 		}
 	} else if (d == "L") {
 		for (int i=1; i<=len; ++i) {
-			x++;
+			x--;
 			std::tuple<int,int> p = std::make_tuple(x, y);
 			coords.push_back(p);
 		}
 	} else if (d == "D") {
 		for (int i=1; i<=len; ++i) {
-			y++;
+			y--;
 			std::tuple<int,int> p = std::make_tuple(x, y);
 			coords.push_back(p);
 		}
@@ -44,11 +56,7 @@ std::vector<std::tuple<int,int>> go(std::tuple<std::string,int> s, std::tuple<in
 		return coords;
 	}
 
-	//std::cout << "> " ;
-	//for (auto it = coords.begin(); it != coords.end(); ++it) {
-		//std::cout << std::get<0>(*it) << "/" << std::get<1>(*it) << " ";
-	//}
-	//std::cout << std::endl;
+	pprint(coords, "go");
 
 	return coords;
 }
@@ -56,25 +64,25 @@ std::vector<std::tuple<int,int>> go(std::tuple<std::string,int> s, std::tuple<in
 std::vector<std::tuple<int,int>> path(std::vector<std::tuple<std::string,int>> ops, std::tuple<int,int> zero)
 {
 	std::vector<std::tuple<int,int>> coords;
+	std::tuple<int,int> start = zero;
 
 	for (uint i=0; i<ops.size(); ++i) {
-		std::tuple<int,int> start;
-		start = zero;
 		std::cout
 			<< ".  i: " << i
 			<< " start: (" << std::get<0>(start) << "/" << std::get<1>(start)
 			<< ") len:" << coords.size()
 			<< std::endl;
 		std::vector<std::tuple<int,int>> c2 = go(ops[i], start);
-		start = coords.back();
 		for (auto it = c2.begin(); it != c2.end(); ++it) {
-			coords.push_back(*c2.data());
+			coords.push_back(*it);
 		}
+		start = coords.back();
 		std::cout
 			<< ".. i: " << i
 			<< " start: (" << std::get<0>(start) << "/" << std::get<1>(start)
 			<< ") len:" << coords.size()
 			<< std::endl;
+		pprint(coords, "path");
 	}
 
 	return coords;
@@ -99,36 +107,79 @@ std::vector<std::tuple<std::string,int>> parse(const std::string & arg)
 	return rv;
 }
 
-int manhattan()
+int manhattan(std::tuple<int,int> p1, std::tuple<int,int> p2)
 {
-	int rv = 0;
-	return rv;
+	int a = std::abs(std::get<0>(p1) - std::get<0>(p2));
+	int b = std::abs(std::get<1>(p1) - std::get<1>(p2));
+	return a + b;
 }
 
-std::tuple<int,int> crossed(std::vector<std::tuple<int,int>> p1, std::vector<std::tuple<int,int>> p, std::tuple<int,int> zero)
+std::vector<std::tuple<int,int>> crossed(std::vector<std::tuple<int,int>> p1, std::vector<std::tuple<int,int>> p2)
 {
-	std::tuple<int,int> result;
+	std::vector<std::tuple<int,int>> tmp;
 
-	return result;
-}
+	int64_t counter = 0;
 
-void pprint(std::vector<std::tuple<int,int>> v)
-{
-	for (auto it = v.begin(); it != v.end(); ++it) {
-		std::tuple<int,int> x = *it;
-		std::cout << "(" << std::get<0>(x) << "/" << std::get<1>(x) << ")";
+	for (auto it1 = p1.begin(); it1 != p1.end(); ++it1) {
+		int x1 = std::get<0>(*it1);
+		int y1 = std::get<1>(*it1);
+
+		for (auto it2 = p2.begin(); it2 != p2.end(); ++it2) {
+			int x2 = std::get<0>(*it2);
+			int y2 = std::get<1>(*it2);
+
+			if (x1 == x2 && y1 == y2) {
+				tmp.push_back(*it1);
+			}
+			++counter;
+			if (counter % 1000000 == 0) std::cout << counter << " | " << tmp.size() << std::endl;
+			if (counter % 1000000000 == 0) {
+				for (auto it3 = tmp.begin(); it3 != tmp.end(); ++it3) {
+					std::cout << " A " << std::get<0>(*it3) << "/" << std::get<1>(*it3);
+				}
+				std::cout << std::endl;
+			}
+		}
 	}
-	std::cout << std::endl;
+	pprint(tmp, "crossed");
+
+	return tmp;
+}
+
+std::tuple<int,int> find(std::vector<std::tuple<int,int>> v, std::tuple<int, int> origin)
+{
+	std::tuple<int,int> rv = origin;
+
+	int lowest = 0;
+
+	for (auto it = v.begin(); it != v.end(); ++it) {
+		int man = manhattan(origin, *it);
+		if (lowest == 0) {
+			lowest = man;
+			rv = *it;
+		} else if (man < lowest) {
+			lowest = man;
+			rv = *it;
+		}
+		std::cout << lowest << std::endl;
+	}
+
+	return rv;
 }
 
 int main(int argc, char *argv[])
 {
 	std::vector<std::string> allops;
 
-	std::string input;
-	while (std::cin) {
-		getline(std::cin, input);
-		if (input.size() > 2) allops.push_back(input);
+	if (argc < 2) {
+		std::string input;
+		while (std::cin) {
+			getline(std::cin, input);
+			if (input.size() > 2) allops.push_back(input);
+		}
+	} else {
+		allops.push_back("R8,U5,L5,D3");
+		allops.push_back("U7,R6,D4,L4");
 	}
 
 	std::cout << "lines: " << allops.size() << std::endl;
@@ -137,15 +188,24 @@ int main(int argc, char *argv[])
 
 	std::tuple<int,int> zero(0, 0);
 
-    std::vector<std::tuple<std::string,int>> ops1 = parse(allops[0]);
+	std::vector<std::tuple<std::string,int>> ops1 = parse(allops[0]);
 	std::vector<std::tuple<int,int>> p1 = path(ops1, zero);
 
 	std::cout << "line 1: " << p1.size() << std::endl;
-	pprint(p1);
+	pprint(p1, "LINE1");
 
-    std::vector<std::tuple<std::string,int>> ops2 = parse(allops[1]);
+	std::vector<std::tuple<std::string,int>> ops2 = parse(allops[1]);
 	std::vector<std::tuple<int,int>> p2 = path(ops2, zero);
 
 	std::cout << "line 2: " << p2.size() << std::endl;
-	pprint(p2);
+	pprint(p2, "LINE2");
+
+	std::vector<std::tuple<int,int>> crx = crossed(p1, p2);
+
+	std::tuple<int,int> rv = find(crx, zero);
+	if (rv == zero) {
+		std::cout << "NOPE" << std::endl;
+	} else {
+		std::cout << "YES " << std::get<0>(rv) << "/" << std::get<1>(rv) << " ~ " << manhattan(rv, zero)  << std::endl;
+	}
 }
