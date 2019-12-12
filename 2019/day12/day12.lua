79a4b40e82aa3eadf04e8b8ac245b5d5f5f3b64c
Florian Anderiasch <fa@art-core.org>
2019-12-12 16:27:57 +0100

    2019 - 12.2 WIP

--- 2019/day12/day12.lua
+++ 2019/day12/day12.lua
@@ -6,7 +6,20 @@ function getl(file)
   return lines
 end
 
-function pp(t) end
+function deepcopy(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in next, orig, nil do
+            copy[deepcopy(orig_key)] = deepcopy(orig_value)
+        end
+        setmetatable(copy, deepcopy(getmetatable(orig)))
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
+end
 
 function rpad(s,l,c)
 	local res = s .. string.rep(c or ' ', l - #s)
@@ -15,28 +28,21 @@ end
 
 function strx(v)
 	local sz = 5
-	return "! " .. rpad(v.name, 10) ..
-	' //  x:' .. rpad(''..v.x, sz) .. 
-	" /  y: " .. rpad(''..v.y, sz) .. 
-	" /  z: " .. rpad(''..v.z, sz) ..
-	' // vx:' .. rpad(''..v.vx,sz) .. 
-	" / vy: " .. rpad(''..v.vy,sz) .. 
-	" / vz: " .. rpad(''..v.vz,sz)
+	return	'! '       .. rpad(v.name, 10) ..
+			' // x: '  .. rpad(''..v.x, sz) ..
+			' / y: '   .. rpad(''..v.y, sz) ..
+			' / z: '   .. rpad(''..v.z, sz) ..
+			' // vx: ' .. rpad(''..v.vx,sz) ..
+			' / vy: '  .. rpad(''..v.vy,sz) ..
+			' / vz: '  .. rpad(''..v.vz,sz)
 end
 
 function world(t)
-	local rv = "=== WORLD\n"
+	local rv = '=== WORLD\n'
 	for k,v in ipairs(t) do
 		rv = rv .. strx(v) .. "\n"
 	end
-	return rv .. "WORLD ==="
-end
-
-function fnd(s, t)
-	for i,v in ipairs(t) do
-		if s == v then return i end
-	end
-	return 0
+	return rv .. 'WORLD ==='
 end
 
 function ppm(t)
@@ -46,7 +52,7 @@ function ppm(t)
 end
 
 function parse(line)
-	x,y,z = string.match(line, "^<x=([^,]+),.*y=([^,]+),.*z=([^,]+)>")
+	x,y,z = string.match(line, '^<x=([^,]+),.*y=([^,]+),.*z=([^,]+)>')
 	return tonumber(x),tonumber(y),tonumber(z)
 end
 
@@ -55,6 +61,30 @@ function calcvelo(moons)
 		moons[k].x = moons[k].x + moons[k].vx
 		moons[k].y = moons[k].y + moons[k].vy
 		moons[k].z = moons[k].z + moons[k].vz
+	end
+	return moons
+end
+
+function calcvelo2(moons, what, vel)
+	for k,_ in ipairs(moons) do
+		moons[k][what] = moons[k][what] + moons[k][vel]
+	end
+	return moons
+end
+
+function calcgrav2(moons, what, vel)
+	for k1,_ in ipairs(moons) do
+		for k2,_ in ipairs(moons) do
+			if k2 <= k1 then goto continue end
+			if moons[k1][what] < moons[k2][what] then
+				moons[k1][vel] = moons[k1][vel] + 1
+				moons[k2][vel] = moons[k2][vel] - 1
+			elseif moons[k1][what] > moons[k2][what] then
+				moons[k1][vel] = moons[k1][vel] - 1
+				moons[k2][vel] = moons[k2][vel] + 1
+			end
+			::continue::
+		end
 	end
 	return moons
 end
@@ -117,10 +147,10 @@ function main()
 
 	lines = getl(fname)
 	moons = {}
-	moons[#moons+1] = { name = "Io" }
-	moons[#moons+1] = { name = "Europa" }
-	moons[#moons+1] = { name = "Ganymede" }
-	moons[#moons+1] = { name = "Callisto" }
+	moons[#moons+1] = { name = 'Io' }
+	moons[#moons+1] = { name = 'Europa' }
+	moons[#moons+1] = { name = 'Ganymede' }
+	moons[#moons+1] = { name = 'Callisto' }
 	if #lines ~= #moons then
 		return 2
 	end
@@ -135,60 +165,95 @@ function main()
 		m.vz = 0
 	end
 	ppm(moons)
-	print("---------------- initialized")
-	
+	print('---------------- initialized')
+
 
 	if not part2 then
-		print("---------------- part1")
-		local steps = 0
+		print('---------------- part1')
 		local len = 1000
+		local steps = 0
 		for i=1,len,1 do
-			moons = calcgrav(moons)
-			moons = calcvelo(moons)
-			steps = steps + 1
-			print("----------------" .. steps .. " steps done")
+			moons = calcgrav2(moons, 'x', 'vx')
+			moons = calcgrav2(moons, 'y', 'vy')
+			moons = calcgrav2(moons, 'z', 'vz')
+
+			moons = calcvelo2(moons, 'x', 'vx')
+			moons = calcvelo2(moons, 'y', 'vy')
+			moons = calcvelo2(moons, 'z', 'vz')
+			steps = i
 		end
+		print('----------------' .. steps .. ' steps done')
 		ppm(moons)
 		e = calcenergy(moons)
 		print(e)
-		print("---------------- part1")
+		print('---------------- part1')
 	else
-		print("---------------- part2")
+		print('---------------- part2')
+		state0 = deepcopy(moons)
+		ppm(state0)
 		local history = {}
-		local history_x = {}
-		local history_y = {}
-		history[#history+1] = world(moons)
-		history_x[#history_x+1] = moons[1].x
-		history_y[#history_y+1] = moons[1].y
-		steps = 0
-		len = 10000000
+		len = 100000
+		len = 4686774924 + 10
+		len = 5000000000
+		----- .  .  .
+		local okx = {}
+		local oky = {}
+		local okz = {}
+		print('---------------- part2 loop: ' .. len)
+		local szt = #state0
 		for i=1,len,1 do
-			moons = calcgrav(moons)
-			moons = calcvelo(moons)
-			steps = steps + 1
-			w = world(moons)
-			f1 = fnd(w, history)
-			if f1 > 0 then
-				print('found:' .. i .. ' ' .. f1)
-				break
+			moons = calcgrav2(moons, 'x', 'vx')
+			moons = calcvelo2(moons, 'x', 'vx')
+			local m = 0
+			for xk,_ in ipairs(state0) do
+				if moons[xk].x ~= state0[xk].x then
+					goto cont1
+				else
+					m = m + 1
+				end
+				:: cont1 ::
 			end
-			history[#history+1] = w
-			history_x[#history_x+1] = moons[1].x
-			history_y[#history_y+1] = moons[1].y
-			--print(#history)
-			if math.fmod(i,10000) == 0 then print(":: " .. i) end
+			if m == szt then okx[#okx+1] = i end
 		end
-		print('len: ' .. #history)
-		--print(history[1])
-		for x,y in ipairs(history) do
-			if x > 2770 and x < 2774 then
-				--print(x)
-				--print(y)
+		for _,i in ipairs(okx) do
+			moons = calcgrav2(moons, 'y', 'vy')
+			moons = calcvelo2(moons, 'y', 'vy')
+			local m = 0
+			for xk,_ in ipairs(state0) do
+				if moons[xk].y ~= state0[xk].y then
+					goto cont2
+				else
+					m = m + 1
+				end
+				:: cont2 ::
 			end
+			if m == szt then oky[#oky+1] = i end
 		end
-		print("---------------- part2")
+		for _,i in ipairs(oky) do
+			moons = calcgrav2(moons, 'z', 'vz')
+			moons = calcvelo2(moons, 'z', 'vz')
+			local m = 0
+			for xk,_ in ipairs(state0) do
+				if moons[xk].z ~= state0[xk].z then
+					goto cont3
+				else
+					m = m + 1
+				end
+				:: cont3 ::
+			end
+			if m == szt then okz[#okz+1] = i end
+		end
+		--[[
+			if math.fmod(i,1000) == 0 then print(':: ' .. i) end
+		]]--
+		print('len: ' .. #okx)
+		print('len: ' .. #oky)
+		print('len: ' .. #okz)
+		for k,x in ipairs(okz) do
+			print(x)
+		end
+		print('---------------- part2')
 	end
 end
-
 
 main()
