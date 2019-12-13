82f2874463e08242a373afa10ed1cc76f7f270c3
Florian Anderiasch <fa@art-core.org>
2019-12-13 18:33:15 +0100

    2019 - 10.1 cleanup

--- 2019/day10/day10.py
+++ 2019/day10/day10.py
@@ -39,55 +39,15 @@ def getvec(x,y,x1,y1):
 def crossp(a, b):
    return a[0] * b[1] - b[0] * a[1];
 
-def intersect(a,b):
-  pass
-
-def angle(a,b):
-  #print("A1",a,b)
-  u = a[0] * b[0] + a[1] * b[1]
-  d = math.sqrt(a[0] * a[0] + a[1] * a[1]) * math.sqrt(b[0] * b[0] + b[1] * b[1])
-  #print("A",u,d)
-  if d == 0:
-    return 0
-  #print("A",u,d,(u/d))
-  rv = math.acos((u / d))
-  return rv
-
 def angle2(a, b):
   return math.atan2(b[1] - a[1], b[0] - a[0])
 
-def uni3(a, pairs):
-  lookup = []
-  npairs = [b for [a,b] in pairs]
-  npairs = list(combinations(npairs, 2))
-  print(len(npairs),"perms")
-  for t in npairs:
-    u0= sub(t[0], a)
-    u1= sub(t[1], a)
-    u0 = t[0]
-    u1 = t[1]
-    x = angle(u0, u1)
-    md = math.degrees(x)
-    if md > 90:
-      md = 180 - md
-    md = round(md,10)
-    print(a,t[0],t[1], md)
-    if not md in lookup:
-      lookup.append(md)
-  return lookup
-
 def uni4(a, pairs):
   lookup = []
-  npairs = [b for [a,b] in pairs]
-  npairs = list(combinations(npairs, 2))
-  print(len(npairs),"perms")
   for t in pairs:
-    u1= sub(t[1], a)
+    u1 = sub(t[1], a)
     x = angle2([0,0], u1)
     md = math.degrees(x)
-    #md = abs(md)
-    #if md > 90:
-    #  md = 180 - md
     md = round(md,10)
     print(a,t[0],t[1], md)
     if not md in lookup:
@@ -114,7 +74,6 @@ def los(m):
       pts.insert(0, ck[0][0])
       print("--",len(pts),"points",pts)
       print("--",len(ck),"pairs",ck)
-      #angles = uni3([x,y], ck)
       angles = uni4([x,y], ck)
       print(sorted(angles))
       print("")
