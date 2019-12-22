f4bcce7c7027b68f36919c0dda73a5ea4cb3818a
Florian Anderiasch <fa@art-core.org>
2019-12-22 08:18:20 +0000

    2019 - 22.2 - WIP

--- 2019/day22/src/main.rs
+++ 2019/day22/src/main.rs
@@ -47,41 +47,16 @@ fn parse(s: &str) -> Step {
 }
 
 fn pp(v: &Vec<u32>) {
-    println!("  cur B {:?} TOP", v);
+    if v.len() < 20 {
+        println!("  cur BOTTOM {:?} TOP", v);
+    } else {
+        println!("  cur BOTTOM [{:?}, {:?}, ... {:?}, {:?}] TOP", v[0], v[1], v[v.len()-2], v[v.len()-1]);
+    }
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let program = args[0].clone();
-    if args.len() < 3 {
-        println!("Usage: {} /path/to/input deck_size", program);
-    }
-    let filename = args[1].clone();
-    let size = args[2].clone();
-    let deck_size: usize = size.parse().unwrap();
-
-    let file = File::open(filename).unwrap();
-    let reader = BufReader::new(file);
-
-    let mut steps: Vec<Step> = Vec::new();
-
-    for (index, line) in reader.lines().enumerate() {
-        let line = line.unwrap();
-        println!("{}> {}", index+1, line.trim());
-        let step = parse(line.trim());
-        //println!("{:?}", step);
-        steps.push(step);
-    }
-
-    let mut deck_orig: Vec<u32> = Vec::with_capacity(deck_size);
-    for i in 0..deck_size {
-        deck_orig.push(i as u32);
-    }
-    deck_orig.reverse();
+fn shuffle(deck_orig: Vec<u32>, steps: Vec<Step>) -> Vec<u32> {
+    let deck_size: usize = deck_orig.len();
     let mut deck_cur = deck_orig.clone();
-    println!("do {:?}", deck_orig);
-    pp(&deck_cur);
-    println!("------------------");
 
     for step in steps {
         println!("Current step: {:?}", step);
@@ -155,14 +130,66 @@ fn main() {
     println!("result:");
     pp(&deck_cur);
 
-    if deck_cur.len() > 2100 {
-        println!("#####");
+    return deck_cur;
+}
+
+fn part1(deck: Vec<u32>) {
+    if deck.len() > 2100 {
         let mut cnt = 1;
-        for ii in (0..deck_cur.len()-1).rev() {
-            if deck_cur[ii] == 2019 {
-                println!("{:?} : {:?} : {:?}", ii, deck_cur[ii], cnt);
+        for ii in (0..deck.len()-1).rev() {
+            if deck[ii] == 2019 {
+                println!("{:?} : {:?} => {:?}", ii, deck[ii], cnt);
             }
             cnt += 1;
         }
+    }
+}
+
+fn part2(steps: Vec<Step>) {}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    if args.len() < 3 {
+        println!("Usage: {} /path/to/input deck_size [2]", program);
+    }
+    let filename = args[1].clone();
+    let size = args[2].clone();
+    let deck_size: usize = size.parse().unwrap();
+
+    let mut part_num: usize = 1;
+    if args.len() > 3 {
+        let part = args[3].clone();
+        part_num = part.parse().unwrap();
+    }
+
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut steps: Vec<Step> = Vec::new();
+
+    for (index, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        println!("{}> {}", index+1, line.trim());
+        let step = parse(line.trim());
+        //println!("{:?}", step);
+        steps.push(step);
+    }
+
+    let mut deck_orig: Vec<u32> = Vec::with_capacity(deck_size);
+    for i in 0..deck_size {
+        deck_orig.push(i as u32);
+    }
+    deck_orig.reverse();
+    let deck_cur = deck_orig.clone();
+    //println!("do {:?}", deck_orig);
+    pp(&deck_cur);
+    println!("------------------");
+
+    if part_num == 2 {
+        part2(steps);
+    } else {
+        let deck_s = shuffle(deck_cur, steps);
+        part1(deck_s);
     }
 }
