2199316b04a38cafcd0c641ee8517e995d0a6f8a
Florian Anderiasch <fa@art-core.org>
2020-01-03 01:38:24 +0100

    2019 - 5.1 + 5.2 - Rust

--- 2019/common/intcode.rs
+++ 2019/common/intcode.rs
@@ -3,7 +3,9 @@ pub struct VM {
     status: i64,
     position: usize,
     code: Vec<i64>,
-    inputs: Vec<i64>
+    inputs: Vec<i64>,
+    outputs: Vec<i64>,
+    debug: bool
 }
 
 impl VM {
@@ -13,38 +15,123 @@ impl VM {
             position: 0,
             code: code,
             inputs: inputs,
+            outputs: Vec::new(),
+            debug: false
+        }
+    }
+
+    pub fn newd(code: Vec<i64>, inputs: Vec<i64>) -> VM {
+        VM {
+            status: 0,
+            position: 0,
+            code: code,
+            inputs: inputs,
+            outputs: Vec::new(),
+            debug: true
+        }
+    }
+
+    pub fn gbm(&mut self, pos: usize, mode: i64) -> i64 {
+        match mode {
+            1 => self.code[pos],
+            _ => { let val = self.code[pos] as usize; self.code[val] }
+        }
+    }
+
+    pub fn gbmo(&mut self, pos: usize, mode: i64) -> i64 {
+        match mode {
+            _ => self.code[pos]
         }
     }
 
     pub fn run(&mut self) {
         let mut finished = false;
         while !finished {
-            //println!("pos: {} = {}", self.position, self.code[self.position]);
-            if self.code[self.position] == 1 {
-                //println!("{} {} {}", self.position+1, self.position+2, self.position+3);
-                let a = self.code[self.position+1] as usize;
-                let b = self.code[self.position+2] as usize;
-                let pos_out = self.code[self.position+3] as usize;
-                let val = self.code[a] + self.code[b];
-                //println!("{} {} = {}", a, b, val);
-                self.code[pos_out] = val;
-            } else if self.code[self.position] == 2 {
-                let a = self.code[self.position+1] as usize;
-                let b = self.code[self.position+2] as usize;
-                let pos_out = self.code[self.position+3] as usize;
-                let val = self.code[a] * self.code[b];
-                self.code[pos_out] = val;
-            } else if self.code[self.position] == 99 {
+            let mut step_size = 4;
+            let mut opcode = self.code[self.position];
+            let par1 = (opcode / 100)   % 10;
+            let par2 = (opcode / 1000)  % 10;
+            let par3 = (opcode / 10000) % 10;
+            opcode = opcode % 100;
+            let params : Vec<i64> = vec![par1, par2, par3];
+            if self.debug { println!("pos: {} opcode: {}", self.position, opcode); }
+            if opcode == 1 { // ADD
+                let a = self.gbm(self.position+1, params[0]);
+                let b = self.gbm(self.position+2, params[1]);
+                let pos_out = self.code[self.position+3];
+                self.code[pos_out as usize] = a + b;
+            } else if opcode == 2 { // MUL
+                let a = self.gbm(self.position+1, params[0]);
+                let b = self.gbm(self.position+2, params[1]);
+                let pos_out = self.code[self.position+3];
+                self.code[pos_out as usize] = a * b;
+            } else if opcode == 3 { // INP
+                let a = self.gbmo(self.position+1, params[0]);
+                match self.inputs.pop() {
+                    Some(x) => self.code[a as usize] = x,
+                    None => {
+                        finished = true;
+                        println!("NO INPUT");
+                    },
+                }
+                step_size = 2;
+            } else if opcode == 4 { // OUT
+                let a = self.gbm(self.position+1, params[0]);
+                self.outputs.push(a);
+                println!("OUTPUT: {}", a);
+                step_size = 2;
+            } else if opcode == 5 { // JMT
+                let a = self.gbm(self.position+1, params[0]);
+                if a != 0 {
+                    let b = self.gbm(self.position+2, params[1]);
+                    self.position = b as usize;
+                    step_size = 0;
+                } else {
+                    step_size = 3;
+                }
+            } else if opcode == 6 { // JMF
+                let a = self.gbm(self.position+1, params[0]);
+                if a == 0 {
+                    let b = self.gbm(self.position+2, params[1]);
+                    self.position = b as usize;
+                    step_size = 0;
+                } else {
+                    step_size = 3;
+                }
+            } else if opcode == 7 { // LT
+                let a = self.gbm(self.position+1, params[0]);
+                let b = self.gbm(self.position+2, params[1]);
+                let pos_out = self.code[self.position+3];
+                if a < b {
+                    self.code[pos_out as usize] = 1;
+                } else {
+                    self.code[pos_out as usize] = 0;
+                }
+            } else if opcode == 8 { //EQ
+                let a = self.gbm(self.position+1, params[0]);
+                let b = self.gbm(self.position+2, params[1]);
+                let pos_out = self.code[self.position+3];
+                if a == b {
+                    self.code[pos_out as usize] = 1;
+                } else {
+                    self.code[pos_out as usize] = 0;
+                }
+            } else if opcode == 99 {
                 finished = true;
             } else {
-                panic!("Unknown code {} at position {}", self.code[self.position], self.position);
+                finished = true;
+                println!("Unknown code {} at position {}", self.code[self.position], self.position);
             }
-            self.position += 4;
+            self.position += step_size;
         }
     }
 
     pub fn code(&mut self) -> Vec<i64> {
         return self.code.clone();
+    }
+
+    pub fn outputs(&mut self) -> Vec<i64> {
+        return self.outputs.clone();
     }
 
     pub fn pp(&mut self) {
---
+++ 2019/day05/src/main.rs
@@ -0,0 +1,67 @@
+extern crate intcode;
+
+use intcode::VM;
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+static DEBUG: bool = false;
+
+
+fn part1(ops: Vec<i64>, inputs: Vec<i64>) -> i64 {
+    let mut vm : VM;
+    if DEBUG {
+        vm = VM::newd(ops, inputs);
+        vm.pp();
+        vm.run();
+        vm.pp();
+    } else {
+        vm = VM::new(ops, inputs);
+        vm.run()
+    }
+    return vm.outputs().last().cloned().unwrap_or(0);
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    if args.len() < 2 {
+        println!("Usage: {} /path/to/input", program);
+        return;
+    }
+    let filename = args[1].clone();
+
+    let mut part_num: usize = 1;
+    if args.len() > 2 {
+        let part = args[2].clone();
+        part_num = part.parse().unwrap();
+    }
+
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    let mut line_iter = reader.lines().map(|l| l.unwrap());
+    let line = line_iter.next().unwrap();
+    let ops_s : Vec<&str> = line.split(",").collect();
+
+    let mut ops : Vec<i64> = Vec::new();
+    let mut op : i64;
+    for s in ops_s {
+            op = s.parse().unwrap();
+            ops.push(op);
+    }
+    //println!("{:?}", ops);
+
+    if part_num == 2 {
+        let inputs: Vec<i64> = vec![5];
+        let p2 = part1(ops, inputs);
+        println!("Part 2: {}", p2);
+    } else if part_num == 0 {
+        let inputs: Vec<i64> = vec![7];
+        let p1 = part1(ops, inputs);
+        println!("Part 0: {}", p1);
+    } else {
+        let inputs: Vec<i64> = vec![1];
+        let p1 = part1(ops, inputs);
+        println!("Part 1: {}", p1);
+    }
+}
