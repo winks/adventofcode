990a348aa68d18cb1329fd690ad4cc1a7fc4cd73
Florian Anderiasch <fa@art-core.org>
2020-12-12 10:04:29 +0100

    2020 - day12 - cleanup

--- 2020/day12/day12.py
+++ 2020/day12/day12.py
@@ -2,7 +2,6 @@ import sys
 import timeit
 
 fname = '../input/day12/input.txt'
-fname = '../input/day12/test'
 
 part1 = True
 if len(sys.argv) > 1 and sys.argv[1] == '2':
@@ -15,26 +14,6 @@ with open(fname) as fh:
 
 def dist(a, b):
     return abs(a) + abs(b)
-
-def pp(x):
-    (a, b) = x
-    if a == 0:
-        if b == 1:
-            return 'N'
-        elif b == -1:
-            return 'S'
-        else: 
-            return 'x'
-    elif a == 1:
-        if b == 0:
-            return 'E'
-        else:
-            return 'y'
-    elif a == -1:
-        if b == 0:
-            return 'W'
-        else:
-            return 'z'
 
 def move(line, cur, fac):
     if line[0] == 'F':
@@ -54,32 +33,7 @@ def move(line, cur, fac):
     return (cur, fac)
 
 def turn(line, fac):
-    mod = int(line[1] / 90)
-    if line[0] == 'L':
-        for x in range(0, mod):
-            if fac == (0, 1):
-                fac = (-1, 0)
-            elif fac == (-1, 0):
-                fac = (0, -1)
-            elif fac == (0, -1):
-                fac = (1, 0)
-            else:
-                fac = (0, 1)
-    elif line[0] == 'R':
-        for x in range(0, mod):
-            if fac == (0, 1):
-                fac = (1, 0)
-            elif fac == (1, 0):
-                fac = (0, -1)
-            elif fac == (0, -1):
-                fac = (-1, 0)
-            else:
-                fac = (0, 1)
-    return fac
-
-def turn2(line, fac):
     mod = int((line[1] % 360) / 90)
-    #bl = [(0, 1), (-1, 0), (0, -1), (1, 0)]
     if mod == 2:
         return (-1 * fac[0], -1 * fac[1])
     elif (line[0] == 'L' and mod == 1) or (line[0] == 'R' and mod == 3):
@@ -88,17 +42,13 @@ def turn2(line, fac):
         return (fac[1], -1 * fac[0])
     return fac
 
-def ori(cur, wpx):
-    pass
-
 def ppart1(lines):
     fac = (1, 0)
     cur = (0, 0)
-    #print(lines)
     for line in lines:
-        (c, f) = move(line, cur, fac)
-        fac = turn2(line, fac)
-        print(line, cur, fac, pp(fac))
+        (cur, _) = move(line, cur, fac)
+        fac = turn(line, fac)
+        #print(line, cur, fac)
     return dist(cur[0], cur[1])
 
 def ppart2(lines):
@@ -108,8 +58,8 @@ def ppart2(lines):
         (c2, wpx) = move(line, cur, wpx)
         if line[0] == 'F':
             cur = c2
-        wpx = turn2(line, wpx)
-        print(line, cur, wpx)
+        wpx = turn(line, wpx)
+        #print(line, cur, wpx)
     return dist(cur[0], cur[1])
 
 start = timeit.default_timer()
