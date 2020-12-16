4f2ebd70ce586cf1dd4fed71c5f9153f5e926233
Florian Anderiasch <fa@art-core.org>
2020-12-16 15:39:53 +0100

    2020 - day16 - part2 cleanup

--- 2020/day16/day16.py
+++ 2020/day16/day16.py
@@ -40,14 +40,10 @@ def ppart1(lines):
         pa = line.split(':')
         px = pa[1].split(' or ')
         rules.append((pa[0], tuple(map(int, px[0].strip().split('-'))), tuple(map(int, px[1].strip().split('-')))))
-    print(rules)
-    print(t_you)
-    print(t_near)
 
     invalid = []
     invalid_tickets = []
     for near in t_near:
-        print(near)
         valid = 0
         tmp = {}
         for n in near:
@@ -60,63 +56,15 @@ def ppart1(lines):
                         tmp[n] = 1
                     else:
                         tmp[n] += 1
-        print(valid, tmp)
-        #tmp = filter(lambda )
         for k in tmp.keys():
             if tmp[k] == len(rules):
                 invalid.append(k)
                 invalid_tickets.append(near)
                 break
-    print(invalid)
     r = 0
     for i in invalid:
         r += i
     return (r, invalid_tickets, rules, t_you, t_near)
-
-def ppart22(inv, rules, t_you, t_near):
-    near2 = []
-    for near in t_near:
-        if near not in inv:
-            near2.append(near)
-    print('----1')
-    print(near2)
-    print('----2')
-    print(rules)
-    print('----3')
-    #mut = list(permutations(map(lambda s: s[0], rules)))
-    #print(mut)
-    print('----4')
-
-    rules2 = {}
-    for i in range(0, len(rules)):
-        name = rules[i][0]
-        rules2[name] = (rules[i][1], rules[i][2])
-
-    ok_m = []
-    #for m in list(mut):
-    for m in permutations(map(lambda s: s[0], rules)):
-        valid_m = 0
-        print(":",m)
-        for near in near2:
-            print(near)
-            valid = 0
-            for i in range(0, len(near)):
-                rule = rules2[m[i]]
-                print(rule, i)
-                if (rule[0][0] <= near[i] <= rule[0][1]) or (rule[1][0] <= near[i] <= rule[1][1]):
-                    valid += 1
-                else:
-                    break
-            if valid == len(near):
-                valid_m += 1
-            else:
-                break
-        if valid_m == len(m):
-            ok_m.append(m)
-        print('--')
-    print('-----')
-    print(ok_m)
-    #if len(ok_m) > 1:
 
 
 def ppart2(inv, rules, t_you, t_near):
@@ -124,36 +72,20 @@ def ppart2(inv, rules, t_you, t_near):
     for near in t_near:
         if near not in inv:
             near2.append(near)
-    print('----1')
-    print(near2)
-    print('----2')
-    print(rules)
-    print('----3')
-    #mut = list(permutations(map(lambda s: s[0], rules)))
-    #print(mut)
     rule_names = list(map(lambda s: s[0], rules))
-    #print(len(mut))
-    print(rule_names)
     rules2 = {}
     for i in range(0, len(rules)):
         name = rules[i][0]
         rules2[name] = (rules[i][1], rules[i][2])
-    print(rules2)
-    print('----4')
 
     ok = []
 
     def ck(ticket, rule):
-        #print(ticket, rule)
         r = []
         for i in range(0, len(ticket)):
             if (rule[0][0] <= ticket[i] <= rule[0][1]) or (rule[1][0] <= ticket[i] <= rule[1][1]):
-                #r.append((i, ticket))
-                ##r.append(i)
                 pass
             else:
-                #r.append((None, None))
-                ##r.append(None)
                 r.append(i)
         return r
 
@@ -163,34 +95,20 @@ def ppart2(inv, rules, t_you, t_near):
         for t in range(0, len(near2)):
             ticket = near2[t]
             v = ck(ticket, rules2[rule_name])
-            print(ticket, rule_name, v)
-            #if (None, None) in v:
-            ##if None in v:
             if len(v) > 0:
                 if rule_name not in banlist:
                     banlist[rule_name] = []
                 banlist[rule_name].append(v[0])
-    print('--------------')
-    print(banlist)
-    for r in banlist.keys():
-        print(r, len(banlist[r]), len(near2))
 
     fixed = []
     idx = len(t_you) - 1
     while idx > 1:
-        print("#", idx)
         for k in banlist.keys():
-            print(',',banlist[k])
             if len(banlist[k]) == idx:
                 a = set(list(range(0, len(t_you))))
                 b = set(banlist[k])
                 fixed.append(( a - b , k))
-            else:
-                print("!!!",idx)
         idx -= 1
-    print('###')
-    print(fixed)
-    print('###')
     fixed2 = [fixed[0]]
     last = None
     acc = set().union(fixed[0][0])
@@ -203,19 +121,13 @@ def ppart2(inv, rules, t_you, t_near):
             for x in f[0]:
                 acc.add(x)
             fixed2.append((ff, f[1]))
-    print('###')
-    print(acc, len(fixed2))
-    print(fixed2)
 
     rv = []
     for i in range(0, len(t_you)):
         for f in fixed2:
-            #print('_',f)
             if list(f[0])[0] == i:
-                print(',',i,f[1]," =",t_you[i])
                 if f[1][0:5] == 'depar':
                     rv.append(t_you[i])
-    print(rv)
     x = 1
     for y in rv:
         x *= y
