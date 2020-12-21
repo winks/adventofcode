93041cfae368ac075da318899b6181ee13e4b5b4
Florian Anderiasch <fa@art-core.org>
2020-12-21 02:42:13 +0100

    2020 - day20 - cleanup

--- 2020/day20/day20.py
+++ 2020/day20/day20.py
@@ -1,5 +1,7 @@
 import sys
 import timeit
+from copy import deepcopy
+import math
 
 fname = '../input/day20/input.txt'
 #fname = '../input/day20/test'
@@ -15,13 +17,13 @@ with open(fname) as fh:
 
 def tiler2(tmp):
     tn = tmp[0]
-    ts = tmp[-1] #[::-1]
+    ts = tmp[-1]
     te = ""
     tw = ""
     for row in tmp:
         tw += row[0]
         te += row[-1]
-    return [tn, te, ts, tw] #[::-1]]
+    return [tn, te, ts, tw]
 
 def rot(tile):
     r1 = tile.copy()
@@ -42,147 +44,48 @@ def flip(tiles):
         tile[2] = tile[2][::-1]
         tile[1], tile[3] = tile[3], tile[1]
         rv.append(tile)
-    """
-    for tile in tiles:
-        for side in range(0, len(tile)):
-            tile[side] = tile[side][::-1]
-        tile[1], tile[3] = tile[3], tile[1]
-        rv.append(tile)
-    """
     return rv
 
-def neigh(x, y, w, h):
-    rv = []
-    direc = ((-1, 0), (0, -1), (0, 1), (1, 0))
-    for d in direc:
-        if not (0 <= x + d[0] < w) or not (0 <= y + d[1] < h):
-            continue
-        rv.append((x + d[0], y + d[1]))
-    return rv
-
-def pp(combos):
-    for c in combos:
-        for tile in c:
-            print(tile)
-        print()
-
-def ppart1(lines):
+def get_tiles(lines):
     cur = 0
     tiles = {}
     tmp = []
-    #print(hash(lines[1]))
-    #print(rev(hash(lines[1])))
     for line in lines:
         if line[0:5] == "Tile ":
             last = cur
             cur = int(line[5:].replace(':',''))
             if len(tmp) > 0:
-                ##print(last, len(tmp))
                 tile = tiler2(tmp)
                 tiles[last] = tile
-                
             tmp = []
         elif len(line) > 0:
             tmp.append(line)
-    
+
     tile = tiler2(tmp)
     tiles[cur] = tile
-    ##print(cur, len(tmp))
-    #print(len(tiles))
-    #print(tiles[2311])
-    #print(rot(tiles[2311]))
-    ##print(flip(rot(tiles[2311])))
+    return tiles
 
+def get_combos(tiles):
     allx = {}
     for k in tiles.keys():
         tile = tiles[k]
         combos = rot(tile)
         combos.extend(flip(combos))
-        #print(len(combos))
         allx[k] = combos
+    return allx
 
-    """
-    gridsize = 3
-    grid = [[0,0,0],[0,0,0],[0,0,0]]
-    grid[0][0] = 1951
-    cur = (0, 0)
-    curtile = allx[1951]
-    #print(curtile)
-    #pp(curtile)
-    for k in allx.keys():
-        if k != 1951:
-            continue
-        nex = neigh(0, 0, gridsize, gridsize)
-        print(cur, nex)
-        for ne in nex:
-            print(cur, ne)
-
-            if ne[0] - cur[0] == 0 and ne[1] - cur[1] == 1:
-                print("right")
-                #print(curtile)
-                for j in allx.keys():
-                    if j == 1951:
-                        continue
-                    for curt in curtile:
-                        #print(curt)
-                        for combos in allx[j]:
-                            if combos[3] == curt[1]:
-                                print(j, combos[3], curt[1])
-                                #return
-            if ne[0] - cur[0] == 1 and ne[1] - cur[1] == 0:
-                print("down")
-                for j in allx.keys():
-                    if j == 1951:
-                        continue
-                    for curt in curtile:
-                        #print(',', curt[2], curt[2] == "#.##...##.")
-                        for combos in allx[j]:
-                            if combos[0] == curt[2]:
-                                print(j, combos[0], curt[2])
-                                print(j)
-                                #return
-            if ne[0] - cur[0] == -1 and ne[1] - cur[1] == 0:
-                print("up")
-            if ne[0] - cur[0] == 0 and ne[1] - cur[1] == -1:
-                print("left")
-    """
-
-    #gridsize = 3
-    """
-    for k in allx.keys():
-        for j in allx.keys():
-            if j == k:
-                continue
-            #nex = neigh(0, 0, gridsize, gridsize)
-            print("# k",k,"j",j)
-            cand = []
-            seen = []
-            for kk in allx[k]:
-                for jj in allx[j]:
-                    if kk[1] == jj[3]:
-                        print("l",k,"r",j)
-                        cand.append((k, j))
-    """
-
+def get_parts(allx):
     meh = {}
     for k in allx.keys():
-        #print(k, allx[k])
         for kk in allx[k]:
             for kkk in kk:
                 if kkk not in meh:
                     meh[kkk] = set()
                 meh[kkk].add(k)
-    #print(len(meh.keys()))
-    #for k in meh.keys():
-    #    print(k, meh[k])
     meh2 = {}
     for k in meh.keys():
         if k[::-1] not in meh2:
             meh2[k] = meh[k]
-    #print(len(meh2.keys()))
-    #for k in meh2.keys():
-    #    print(k, meh2[k])
-
     cnt = {}
     for k in allx.keys():
         if k not in cnt:
@@ -190,59 +93,27 @@ def ppart1(lines):
         for j in meh2.keys():
             if len(meh2[j]) == 2 and k in meh2[j]:
                 cnt[k] += 1
-    #print(cnt)
+    return cnt
+
+def ppart1(lines):
+    tiles = get_tiles(lines)
+    allx = get_combos(tiles)
+    cnt = get_parts(allx)
+
     corners = list(filter(lambda s: cnt[s] == 2, cnt.keys()))
-    print("# corners:", len(corners),"#", corners)
-    sides = list(filter(lambda s: cnt[s] == 3, cnt.keys()))
-    print("# sides  :", len(sides),"#", sides)
-    rest = set(allx.keys())
-    rest = list(rest - set(corners) - set(sides))
-    print("# rest   :", len(rest),"#", rest)
+    #print("# corners:", len(corners),"#", corners)
+    #sides = list(filter(lambda s: cnt[s] == 3, cnt.keys()))
+    #print("# sides  :", len(sides),"#", sides)
+    #rest = list(set(allx.keys()) - set(corners) - set(sides))
+    #print("# rest   :", len(rest),"#", rest)
 
     rv = 1
     for c in corners:
         rv *= c
     return rv
 
-
-
-
 def ppart2(lines):
-    def conv(s):
-        if s == 'x':
-            return 0
-        return int(s)
-    
-    tt0 = list(map(conv,lines[1].split(',')))
-    indi = []
-    i = 0
-    for x in tt0:
-        if x > 0:
-            indi.append((i, x))
-        i += 1
-
-    ts = 13561262363414 * 41 # 556...
-    ts = 2439024390243 * 41 # 1000...
-    ts0 = ts
-    rx = indi[0][1] * indi[1][1]
-    while True:
-        if ts % indi[0][1] == 0:
-            if (ts + indi[1][0]) % indi[1][1] == 0:
-                ts0 = ts
-                break
-        ts += indi[0][1]
-
-    while True:
-        ts += rx
-        ok = True
-        for i in indi[2:]:
-            if (ts + i[0]) % i[1] != 0:
-                ok = False
-                break
-        if ok:
-            return ts
-    return 0
-
+    pass
 
 start = timeit.default_timer()
 if part1:
@@ -251,44 +122,7 @@ if part1:
     print("#", (end - start) * 1000)
     print(num)
 else:
-
     num = ppart2(lines)
     end = timeit.default_timer()
     print("#", (end - start) * 1000)
     print(num)
-
-"""
-
-def hash(line):
-    rv = ""
-    cur = line[0]
-    #print("---", line)
-    for i in range(1, len(line)):
-        #print(i, cur,rv)
-        if line[i] != cur[0]:
-            rv += str(len(cur)) + cur[0]
-            cur = line[i]
-        else:
-            cur += line[i]
-    return rv
-
-def rev(hash):
-    rv = ""
-    for i in range(0, len(hash)):
-        if i % 2 == 0:
-            rv = hash[i] + hash[i+1] + rv
-            i +=1
-    return rv
-
-def tiler(tmp):
-    tn = hash(tmp[0])
-    ts = hash(tmp[-1])
-    te = ""
-    tw = ""
-    for row in tmp:
-        tw += row[0]
-        te += row[-1]
-    te = hash(te)
-    tw = hash(tw)
-    return [tn, te, ts, tw]
-"""
