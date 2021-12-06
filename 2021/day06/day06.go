1ee2b371a7b651f6c1cfb7dd41780d433c82f7f9
Florian Anderiasch <fa@art-core.org>
2021-12-06 09:11:46 +0100

    2021 - day06 - go - cleanup

--- 2021/day06/day06.go
+++ 2021/day06/day06.go
@@ -20,14 +20,6 @@ func check(e error) {
 	}
 }
 
-func numArg(s string, def int) int {
-	stx, err := strconv.Atoi(s)
-	if err != nil {
-		return def
-	}
-	return stx
-}
-
 func getLines(filename string) []string {
 	fh, err := os.OpenFile(filename, os.O_RDONLY, os.ModePerm)
 	check(err)
@@ -51,38 +43,7 @@ func count(fish []School) int {
 	return rv
 }
 
-func part1(lines []string) int {
-	lines2 := strings.Split(lines[0], ",")
-	fish := []int{}
-	for _, v := range(lines2) {
-		n, err := strconv.Atoi(v)
-		check(err)
-		fish = append(fish, n)
-	}
-
-	done := false
-	day := 0
-	for !done {
-		for i, f := range(fish) {
-			if f > 0 {
-				f -= 1
-			} else if f == 0 {
-				f = 6
-				fish = append(fish, 8)
-			} else {
-				fmt.Printf("ERR %v\n", f)
-			}
-			fish[i] = f
-		}
-		day +=1
-		if day >= 80 {
-			done = true
-		}
-	}
-	return len(fish)
-}
-
-func part2(lines []string) int {
+func run(lines []string, days int) int {
 	lines2 := strings.Split(lines[0], ",")
 	fish := []School{}
 	for _, v := range(lines2) {
@@ -91,10 +52,8 @@ func part2(lines []string) int {
 		f := School{1, n}
 		fish = append(fish, f)
 	}
-
-	done := false
 	day := 0
-	for !done {
+	for {
 		spawn := 0
 		for i, f := range(fish) {
 			if f.tick > 0 {
@@ -112,11 +71,10 @@ func part2(lines []string) int {
 			fish = append(fish, nf)
 		}
 		day +=1
-		if day >= 256 {
-			done = true
+		if day >= days {
+			return count(fish)
 		}
 	}
-	return count(fish)
 }
 
 func main() {
@@ -133,11 +91,11 @@ func main() {
 	elapsed := time.Since(timeStart)
 	fmt.Printf("# Parsing %s\n", elapsed)
 	timeStart1 := time.Now()
-	p1 := part1(lines)
+	p1 := run(lines, 80)
 	elapsed1 := time.Since(timeStart1)
 	fmt.Printf("# Part1   %s\n", elapsed1)
 	timeStart2 := time.Now()
-	p2 := part2(lines)
+	p2 := run(lines, 256)
 	elapsed2 := time.Since(timeStart2)
 	fmt.Printf("# Part2   %s\n", elapsed2)
 	fmt.Printf("# Total   %s\n", time.Since(timeStart))
