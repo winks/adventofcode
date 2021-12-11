8ec61633a299bd270f647517cc8180cd4b87eb8a
Florian Anderiasch <fa@art-core.org>
2021-12-11 23:46:00 +0100

    2021 - day06 - clojure - cleanup

--- 2021/clojure/src/aoc/day06.clj
+++ 2021/clojure/src/aoc/day06.clj
@@ -7,30 +7,31 @@
     tank
     (let [f (first fish)
           h (take f tank)
-          t (take-last (- 8 f) tank)
-          x (conj t (inc (nth tank f)) h)
-          tank2 (flatten x)]
-    (prep tank2 (rest fish)))))
+          t (take-last (- 8 f) tank)]
+    (prep (flatten [h (inc (nth tank f)) t]) (rest fish)))))
 
-(defn tick1 [tank]
+(defn tick [tank]
   (let [t (rest tank)]
     (if (> (first tank) 0)
       (flatten [(take 6 t) (+ (first tank) (nth t 6)) (nth t 7) (first tank)])
       (flatten [t 0]))))
 
-(defn help1 [tank runs]
+(defn help [tank runs]
   (if (= 0 runs)
     (reduce + tank)
-    (let [tank2 (tick1 tank)]
-      (help1 tank2 (dec runs)))))
+    (help (tick tank) (dec runs))))
 
 (defn part1 [lines runs]
   (let [fish (map #(Integer/parseInt %) (str/split (first lines) #","))
-        tank (vec (repeat 8 0))
-        tank2 (prep tank fish)]
-    (help1 tank2 runs)))
+        tank (prep (vec (repeat 8 0)) fish)]
+    (help tank runs)))
 
 (defn run [filename]
   (let [lines (lib/lines-str filename)]
     (time (println "Part 1: " (part1 lines 80)))
-    (time (println "Part 2: " (part1 lines 256)))))
+    (time (println "Part 2: " (part1 lines 256)))
+    (let [fish (map #(Integer/parseInt %) (str/split (first lines) #","))
+          tank (prep (vec (repeat 8 0)) fish)]
+      (time (println (help tank 80)))
+      (time (println (help tank 256)))
+    )))
