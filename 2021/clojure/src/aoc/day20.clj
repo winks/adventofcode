12deb4221814bdc0c96b8f5c0a720e00621f9db6
Florian Anderiasch <fa@art-core.org>
2021-12-20 16:51:39 +0100

    2021 - day20 - clojure - comments

--- 2021/clojure/src/aoc/day20.clj
+++ 2021/clojure/src/aoc/day20.clj
@@ -1,50 +1,48 @@
 (ns aoc.day20
   (:require [aoc.lib :as lib]))
 
-(defn pp [im]
+(defn pp "pretty print" [im]
   (doall (for [row im] (println row)))
   (println "--"))
 
-(defn pad [im lrow trow offset]
+(defn pad "pad the image with a margin of <offset> dots all around" [im lrow trow offset]
   (let [mid (for [row im] (str lrow row lrow))]
   (concat (repeat offset trow) mid (repeat offset trow))))
 
-(defn exp [x n]
+(defn exp "exponent - x ^ n" [x n]
   (reduce * (repeat n x)))
 
-(defn bin [s rv i]
+(defn bin "convert string of '.#' to binary, .#.# = 0101 = 5" [s rv n]
   (if (empty? s)
     rv
     (let [pos (dec (count s))
           last (subs s pos)
           rest (subs s 0 pos)
-          plus (if (= "#" last) (exp 2 i) 0)]
-    (bin rest (+ rv plus) (inc i)))))
+          plus (if (= "#" last) (exp 2 n) 0)]
+    (bin rest (+ rv plus) (inc n)))))
 
-(defn new-tile [image x y algo]
+(defn new-tile "get algo pos of new tile at row y, column x" [image x y]
   (let [line-len (count (nth image y))
         firstx (if (<= x 1) 0 (dec x))
         lastx  (if (<= x (- line-len 3)) (+ 2 x) (- line-len 1))
         top (if (= 0 y) ""                   (subs (nth image (dec y)) firstx lastx))
         mid                                  (subs (nth image      y)  firstx lastx)
-        bot (if (= (dec( count image)) y) "" (subs (nth image (inc y)) firstx lastx))
-        s (str top mid bot)
-        pos (bin s 0 0)]
-  (nth algo pos)))
+        bot (if (= (dec( count image)) y) "" (subs (nth image (inc y)) firstx lastx))]
+  (bin (str top mid bot) 0 0)))
 
-(defn step [image algo]
-  (let [ll (count (first image))]
+(defn step "proceed one step by going through the image by rows" [image algo]
+  (let [line-len (count (first image))]
     (for [y (range (count image))]
       (loop [x 1 row2 ""]
-        (if (> x (- ll 3))
-          (if (= \. (first row2)) (str \. row2 \. \.) (str \# row2 \# \#))
-          (recur (inc x) (str row2 (new-tile image x y algo))))))))
+        (if (> x (- line-len 3))
+          (if (= \. (first row2)) (str \. row2 \. \.) (str \# row2 \# \#)) ; pretend the margin is infinity
+          (recur (inc x) (str row2 (nth algo (new-tile image x y)))))))))
 
-(defn counter [image]
+(defn counter "count the # in an image" [image]
   (let [s (for [row image] (count (clojure.string/replace row #"\." "")))]
     (reduce + s)))
 
-(defn part1 [image algo runs]
+(defn part1 "part1, actually also part2" [image algo runs]
   (let [offset (+ 5 runs)
         lrow (clojure.string/join (repeat offset \.))
         trow (str lrow (clojure.string/join (repeat (count (first image)) \.)) lrow)
