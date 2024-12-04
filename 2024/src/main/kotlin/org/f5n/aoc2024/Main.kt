3bf63443734871b9d69389d36a0ed7080b3ecfeb
Florian Anderiasch <fa@art-core.org>
2024-12-04 21:47:01 +0100

    2024: Improve runner

--- 2024/src/main/kotlin/org/f5n/aoc2024/Main.kt
+++ 2024/src/main/kotlin/org/f5n/aoc2024/Main.kt
@@ -7,26 +7,28 @@ fun main(args: Array<String>) {
         println("Usage: dayXX [FILENAME]")
         exitProcess(0)
     }
+    val day = if (args[0].length < 5) "day0${args[0].replace("day", "")}" else args[0]
+    val inputFile = if (args.size < 2) "input/${day}/input.txt" else args[1]
 
-    if (args[0] == "day01") {
+    if (day == "day01") {
         val d = Day01()
-        d.run(arrayOf(args[1]))
-        d.run2(arrayOf(args[1]))
-    } else if (args[0] == "day02") {
+        d.run(arrayOf(inputFile))
+        d.run2(arrayOf(inputFile))
+    } else if (day == "day02") {
         val d = Day02()
-        d.run(arrayOf(args[1]))
-        d.run(arrayOf(args[1]), true)
-    } else if (args[0] == "day03") {
+        d.run(arrayOf(inputFile))
+        d.run(arrayOf(inputFile), true)
+    } else if (day == "day03") {
         val d = Day03()
-        d.run(arrayOf(args[1]))
-    } else if (args[0] == "day04") {
+        d.run(arrayOf(inputFile))
+    } else if (day == "day04") {
         val d = Day04()
-        d.run(arrayOf(args[1]))
-    } else if (args[0] == "day05") {
+        d.run(arrayOf(inputFile))
+    } else if (day == "day05") {
         val d = Day05()
-        d.run(arrayOf(args[1]))
-    } else if (args[0] == "day06") {
+        d.run(arrayOf(inputFile))
+    } else if (day == "day06") {
         val d = Day06()
-        d.run(arrayOf(args[1]))
+        d.run(arrayOf(inputFile))
     }
 }
