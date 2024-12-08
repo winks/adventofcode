207884f5dace7ebfe94e373e14c737c27b023360
Florian Anderiasch <fa@art-core.org>
2024-12-08 09:29:44 +0100

    2024: day05 cleanup

--- 2024/src/main/kotlin/org/f5n/aoc2024/Day05.kt
+++ 2024/src/main/kotlin/org/f5n/aoc2024/Day05.kt
@@ -1,21 +1,26 @@
 package org.f5n.aoc2024
 
 class Day05 {
+    sealed class Result {
+        data class Found(val i: Int, val j: Int) : Result()
+        data object Ok : Result()
+    }
+
     fun run(args: Array<String>) {
         val lines = args[0].readLines()
         val rules: MutableMap<Pair<Int, Int>, Int> = mutableMapOf()
         var s2 = false
         var result1 = 0
         var result2 = 0
-        val iv = fun(ord: List<Int>, rules: MutableMap<Pair<Int, Int>, Int>): Pair<Int, Int>? {
+        val iv = fun(ord: List<Int>, rules: MutableMap<Pair<Int, Int>, Int>): Result {
             for (i in ord.indices) {
                 for (j in i + 1 until ord.size) {
                     if (rules.containsKey(Pair(ord[i], ord[j]))) {
-                        return Pair(i, j)
+                        return Result.Found(i, j)
                     }
                 }
             }
-            return null
+            return Result.Ok
         }
         lines.forEach {
             if (it.isEmpty()) {
@@ -27,21 +32,24 @@ class Day05 {
             } else {
                 if (it.isEmpty()) return@forEach
                 val ord = it.split(",").map { it.toInt() }.toTypedArray()
-                var check: Pair<Int, Int>? = iv(ord.toList(), rules)
-                if (check == null) {
-                    result1 += ord[(ord.size - 1) / 2]
-                } else {
-                    //println("\n# ${ord.toList()} $check")
-                    var i = 1
-                    while (check != null) {
-                        val a = check.first
-                        val b = check.second
-                        val tmp = ord[b]
-                        ord[b] = ord[a]
-                        ord[a] = tmp
-                        check = iv(ord.toList(), rules)
+                var check: Result = iv(ord.toList(), rules)
+                when (check) {
+                    Result.Ok -> {
+                        result1 += ord[(ord.size - 1) / 2]
                     }
-                    result2 += ord[(ord.size - 1) / 2]
+
+                    else -> {
+                        //println("\n# ${ord.toList()} $check")
+                        while (check is Result.Found) {
+                            val a = check.i
+                            val b = check.j
+                            val tmp = ord[b]
+                            ord[b] = ord[a]
+                            ord[a] = tmp
+                            check = iv(ord.toList(), rules)
+                        }
+                        result2 += ord[(ord.size - 1) / 2]
+                    }
                 }
             }
         }
