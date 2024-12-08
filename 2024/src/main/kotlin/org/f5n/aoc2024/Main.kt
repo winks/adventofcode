fd8b2d97016c00e608895b65bca5d4104c289271
Florian Anderiasch <fa@art-core.org>
2024-12-08 01:14:34 +0100

    2024: runtime

--- 2024/src/main/kotlin/org/f5n/aoc2024/Main.kt
+++ 2024/src/main/kotlin/org/f5n/aoc2024/Main.kt
@@ -1,5 +1,6 @@
 package org.f5n.aoc2024
 
+import kotlin.time.measureTime
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
@@ -10,31 +11,36 @@ fun main(args: Array<String>) {
     val day = if (args[0].length < 5) "day0${args[0].replace("day", "")}" else args[0]
     val inputFile = if (args.size < 2) "input/${day}/input.txt" else args[1]
 
-    if (day == "day01") {
-        val d = Day01()
-        d.run(arrayOf(inputFile))
-        d.run2(arrayOf(inputFile))
-    } else if (day == "day02") {
-        val d = Day02()
-        d.run(arrayOf(inputFile))
-        d.run(arrayOf(inputFile), true)
-    } else if (day == "day03") {
-        val d = Day03()
-        d.run(arrayOf(inputFile))
-    } else if (day == "day04") {
-        val d = Day04()
-        d.run(arrayOf(inputFile))
-    } else if (day == "day05") {
-        val d = Day05()
-        d.run(arrayOf(inputFile))
-    } else if (day == "day06") {
-        val d = Day06()
-        d.run(arrayOf(inputFile))
-    } else if (day == "day07") {
-        val d = Day07()
-        d.run(arrayOf(inputFile))
-    } else if (day == "day07b") {
-        val d = Day07b()
-        d.run(arrayOf(inputFile))
-    }
+    measureTime {
+        if (day == "day01") {
+            val d = Day01()
+            d.run(arrayOf(inputFile))
+            d.run2(arrayOf(inputFile))
+        } else if (day == "day02") {
+            val d = Day02()
+            d.run(arrayOf(inputFile))
+            d.run(arrayOf(inputFile), true)
+        } else if (day == "day03") {
+            val d = Day03()
+            d.run(arrayOf(inputFile))
+        } else if (day == "day04") {
+            val d = Day04()
+            d.run(arrayOf(inputFile))
+        } else if (day == "day05") {
+            val d = Day05()
+            d.run(arrayOf(inputFile))
+        } else if (day == "day06") {
+            val d = Day06()
+            d.run(arrayOf(inputFile))
+        } else if (day == "day07") {
+            val d = Day07()
+            d.run(arrayOf(inputFile))
+        } else if (day == "day07b") {
+            val d = Day07b()
+            d.run(arrayOf(inputFile))
+        } else if (day == "day08") {
+            val d = Day08()
+            d.run(arrayOf(inputFile))
+        }
+    }.let { println(it) }
 }
