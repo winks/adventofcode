0264b0b8d36e1ae44fc102f89a5e55bafa01cf4c
Florian Anderiasch <fa@art-core.org>
2024-12-09 08:37:45 +0100

    2024: day09 part 1

--- 2024/src/main/kotlin/org/f5n/aoc2024/Day09.kt
+++ 2024/src/main/kotlin/org/f5n/aoc2024/Day09.kt
@@ -1,42 +1,53 @@
 package org.f5n.aoc2024
 
 class Day09 {
-	fun run(args: Array<String>) {
-		val lines = args[0].readLines()[0]
-		println(lines)
-		var file = true
-		var id = 0
-		var fs2 = ""
+    fun run(args: Array<String>) {
+        val lines = args[0].readLines()[0]
+        println(lines)
+        var file = true
+        var id = 0
+        val fs = emptyList<Long>().toMutableList()
 
-		for (c in lines) {
-			if (file) {
-				fs2 += "$id".repeat("$c".toInt())
-				id++
-			} else {
-				fs2 += ".".repeat("$c".toInt())
-			}
-			file = !file
-		}
-		println(fs2)
-		val re = Regex("\\.+\\d+")
-		while (re.find(fs2) != null) {
-			val pos = fs2.indexOfFirst{ it == '.'}
-			val last = fs2.indexOfLast { it != '.' }
-			val fs = fs2.substring(0, pos) + fs2.elementAt(last) + fs2.substring(pos+1, last) + '.' + fs2.substring(last+1)
-			fs2 = fs
-		}
-		println(fs2.length)
-		println(fs2)
-		var p1 = 0L
-		for (c in fs2.indices) {
-			if (fs2.elementAt(c) == '.') {
-				continue
-			}
-			val r : Long = c * fs2.elementAt(c).toString().toLong()
-			p1 += r
-		}
-	println()
-		println(p1)
-		// 92349417108
-	}
+        for (c in lines) {
+            if (file) {
+                for (i in 0 until "$c".toInt()) {
+                    fs += id.toLong()
+                }
+                id++
+            } else {
+                for (i in 0 until "$c".toInt()) {
+                    fs += -1
+                }
+            }
+            file = !file
+        }
+        println(fs)
+        val hasSpace = fun(x: List<Long>): Boolean {
+            val sp = x.indexOfFirst { it == -1L }
+            for (i in sp until x.size) {
+                if (i > sp && x[i] > -1L) {
+                    return true
+                }
+            }
+            return false
+        }
+        while (hasSpace(fs)) {
+            val pos = fs.indexOfFirst { it == -1L }
+            val last = fs.indexOfLast { it > -1L }
+            val x2 = fs[last]
+            fs[pos] = x2
+            fs[last] = -1L
+        }
+        println(fs)
+        var p1 = 0L
+        for (c in fs.indices) {
+            if (fs.elementAt(c) == -1L) {
+                continue
+            }
+            val r: Long = c * fs.elementAt(c)
+            p1 += r
+        }
+        println()
+        println(p1)
+    }
 }
