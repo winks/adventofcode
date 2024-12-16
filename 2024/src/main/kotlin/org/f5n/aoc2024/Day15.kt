f299e959f1eea69c6f3717c8af0f62d3263c210e
Florian Anderiasch <fa@art-core.org>
2024-12-16 21:55:17 +0100

    2024: day 15 part 2 - only test input works

--- 2024/src/main/kotlin/org/f5n/aoc2024/Day15.kt
+++ 2024/src/main/kotlin/org/f5n/aoc2024/Day15.kt
@@ -1,13 +1,18 @@
 package org.f5n.aoc2024
 
+import kotlin.math.abs
+
 class Day15 {
     val BOX = 'O'
     val WALL = '#'
     val UP = '^'
+    val DOWN = 'v'
     val LEFT = '<'
     val RIGHT = '>'
+    val BOXL = '['
+    val BOXR = ']'
 
-    fun move(board: Board, step: Char) : Board {
+    private fun move(board: Board, step: Char, fn: (Board, Char, List<Pos>, Pos) -> Board) : Board {
         val robot = board.getMatches("@")[0]
         val line =
             when (step) {
@@ -22,10 +27,47 @@ class Day15 {
             board.board[nxt.y][nxt.x] = '@'
             return board
         }
-        return box(board, step, line, nxt)
+        return fn(board, step, line, nxt)
     }
 
     fun box(board: Board, step: Char, line: List<Pos>, nxt: Pos) : Board {
+        val robot = board.getMatches("@")[0]
+        val rest = if (step == LEFT || step == UP) {
+            val r = line.subList(0, line.indexOf(nxt))
+            val s = r.lastOrNull { board.peek(it) == WALL }
+            if (s != null) {
+                r.subList(r.indexOf(s), r.size)
+            } else {
+                r
+            }
+        } else {
+            val r = line.subList(line.indexOf(nxt), line.size)
+            val s = r.firstOrNull { board.peek(it) == WALL }
+            if (s != null) {
+                r.subList(0, r.indexOf(s))
+            } else {
+                r
+            }
+        }
+        val free = if (step == LEFT || step == UP) rest.lastOrNull { board.peek(it) == '.' } else rest.firstOrNull { board.peek(it) == '.' }
+        if (free == null) {
+            return board
+        } else {
+            val nxt2 = line[line.indexOf(nxt) + if (step == LEFT || step == UP) -1 else 1]
+            if (board.peek(nxt2) == '.') {
+                board.board[robot.y][robot.x] = '.'
+                board.board[nxt.y][nxt.x] = '@'
+                board.board[nxt2.y][nxt2.x] = BOX
+            } else {
+                board.board[robot.y][robot.x] = '.'
+                board.board[nxt.y][nxt.x] = '@'
+                board.board[free.y][free.x] = BOX
+            }
+            return board
+        }
+    }
+
+    fun box2(board: Board, step: Char, line: List<Pos>, nxt: Pos) : Board {
         val robot = board.getMatches("@")[0]
         val rest = if (step == LEFT || step == UP) {
             val r = line.subList(0, line.indexOf(nxt))
@@ -46,37 +88,124 @@ class Day15 {
                 r
             }
         }
-//        println(line)
-//        println("$robot $nxt")
-//        println(rest)
         val free = if (step == LEFT || step == UP) rest.lastOrNull { board.peek(it) == '.' } else rest.firstOrNull { board.peek(it) == '.' }
         if (free == null) {
             // do nothing
             return board
         } else {
-            val nxt2 = line[line.indexOf(nxt) + if (step == LEFT || step == UP) -1 else 1]
-            if (board.peek(nxt2) == '.') {
-                board.board[robot.y][robot.x] = '.'
-                board.board[nxt.y][nxt.x] = '@'
-                board.board[nxt2.y][nxt2.x] = BOX
-            } else {
-                board.board[robot.y][robot.x] = '.'
-                board.board[nxt.y][nxt.x] = '@'
-                board.board[free.y][free.x] = BOX
+            when (step) {
+                LEFT -> {
+                    var left = true
+                    for (i in free.x until robot.x-1) {
+                        board.board[free.y][i] = if (left) BOXL else BOXR
+                        left = !left
+                    }
+                    board.board[robot.y][robot.x] = '.'
+                    board.board[nxt.y][nxt.x] = '@'
+                }
+                RIGHT -> {
+                    var left = false
+                    for (i in robot.x+1 until free.x+1) {
+                        board.board[free.y][i] = if (left) BOXL else BOXR
+                        left = !left
+                    }
+                    board.board[robot.y][robot.x] = '.'
+                    board.board[nxt.y][nxt.x] = '@'
+                }
+                UP, DOWN -> {
+                    println("free $free - robot $robot")
+                    val todo = mutableSetOf(robot)
+                    var loop_start = if (step == UP) robot.y-1 else robot.y+1
+                    var loop_end = if (step == UP) 0 else board.length
+                    val offset = if (step == UP) 1 else -1
+                    var yy = loop_start
+                    while (yy != loop_end) {
+                        val li = todo.toList().sortedBy { it.x }.filter { it.y == yy+offset }
+                        println("prep: $yy ($loop_start, $loop_end) li $li")
+                        if (abs(yy - robot.y) == 1) {
+                            if (board.board[yy][robot.x] == BOXL) {
+                                println(12301)
+                                todo.add(Pos(robot.x, yy))
+                            } else {
+                                println(12302)
+                                todo.add(Pos(robot.x-1, yy))
+                            }
+                            todo.remove(robot)
+                            println("robot todo $todo")
+                        } else {
+                            for(ll in li) {
+                                println("${ll} : $li")
+                                // []
+                                // []
+                                if (board.board[yy][ll.x] == BOXL) {
+                                    println(9982)
+                                    todo.add(Pos(ll.x, yy))
+                                }
+                                // []
+                                //  []
+                                if (board.board[yy][ll.x] == BOXR ) {
+                                    println(9983)
+                                    todo.add(Pos(ll.x-1, yy))
+                                }
+                                //  []
+                                // []
+                                if (board.board[yy][ll.x+1] == BOXL) {
+                                    println(9984)
+                                    todo.add(Pos(ll.x+1, yy))
+                                }
+                            }
+                        }
+                        if (step == UP) yy-- else yy++
+                    }
+                    val d1 = todo.sortedBy { it.y }.first().y
+                    val d2 = todo.sortedBy { it.y }.last().y
+                    println("end todo $todo diff:${abs(d1-d2)}")
+                    loop_start = if (step == DOWN) board.length else 0
+                    loop_end = if (step == DOWN) robot.y+1 else robot.y-offset
+                    yy = loop_start
+                    while (yy != loop_end) {
+                        println("print: $yy ($loop_start, $loop_end)")
+                        val li = todo.toList().filter { it.y == yy+offset }
+                        println("li $li")
+                        for(a in li) {
+                            if (board.board[yy][a.x] == WALL || board.board[yy][a.x+1] == WALL) {
+                                println("abort1: wall at ${a.x}, ${yy} during $step")
+                                return board
+                            }
+                            if (board.board[yy][a.x+1] == BOXL && !todo.contains(Pos(a.x+1, yy))) {
+                                println("abort2: box at ${a.x+1}, ${yy} during $step")
+                                return board
+                            }
+                            if (board.board[yy][a.x] == BOXR && !todo.contains(Pos(a.x-1, yy))) {
+                                println("abort3: box at ${a.x+1}, ${yy} during $step")
+                                return board
+                            }
+                        }
+                        for(a in li) {
+                            board.board[yy][a.x] = board.board[yy+offset][a.x]
+                            board.board[yy][a.x+1] = board.board[yy+offset][a.x+1]
+                            board.board[yy+offset][a.x] = '.'
+                            board.board[yy+offset][a.x+1] = '.'
+                        }
+                        if (step == UP) yy++ else yy--
+                    }
+                    board.board[robot.y][robot.x] = '.'
+                    board.board[nxt.y][nxt.x] = '@'
+                }
+                else -> {
+
+                }
             }
             return board
         }
+        return board
     }
 
     fun run(args: Array<String>) {
         val lines = args[0].readAll().split("\n\n")
         var board = Board(lines[0].split("\n").toTypedArray())
-//        board.print()
         for (step in lines[1].replace("\n", "")) {
-//            println("step: $step")
-            board = move(board, step)
-//            board.print()
-//            println()
+            board = move(board, step, ::box)
         }
         var p1 = 0
         for (y in 0 until board.length) {
@@ -87,5 +216,34 @@ class Day15 {
             }
         }
         println("p1 : $p1")
+    }
+    fun run2(args: Array<String>) {
+        val lines = args[0].readAll().split("\n\n")
+        val line = lines[0]
+            .replace("#","##")
+            .replace(".", "..")
+            .replace("@", "@.")
+            .replace("O", "[]")
+        var board = Board(line.split("\n").toTypedArray())
+        board.print()
+        var i = 0
+        for (step in lines[1].replace("\n", "")) {
+            println("step: $step step $i")
+            board = move(board, step, ::box2)
+            println()
+            // 1558309 too low
+            // 1548973 too low
+            i++
+        }
+        board.print()
+        var p2 = 0
+        for (y in 0 until board.length) {
+            for (x in 0 until board.width) {
+                if (board.board[y][x] == BOXL) {
+                    p2 += 100 * y + x
+                }
+            }
+        }
+        println("p2 : $p2")
     }
 }
